b2ca6552c83c8c106d035742d0bd681f0a9b0470
XMU-smartdsp <xmu-smartdsp@hotmail.com>
2019-09-16 14:53:47 +0800

    Update plot_conf.py

--- plot_conf.py
+++ plot_conf.py
@@ -6,8 +6,8 @@ Created on Wed Apr 10 11:03:56 2019
 """
 
 
-from sklearn.metrics import confusion_matrix    # 生成混淆矩阵函数
-import matplotlib.pyplot as plt    # 绘图库
+from sklearn.metrics import confusion_matrix    # generate confusion matrix
+import matplotlib.pyplot as plt    # plot lib
 import matplotlib as mat
 import numpy as np
 #import tensorflow as tf
@@ -17,20 +17,20 @@ zhfont1 = mat.font_manager.FontProperties(fname='C:/Windows/Fonts/simsun.ttc')
 def plot_confusion_matrix(cm, title):
     cm_o = cm
     plt.rcParams['figure.figsize'] = (12.0, 10.0)
-    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]    # 归一化
-    plt.imshow(cm, interpolation='nearest')    # 在特定的窗口上显示图像
-    plt.title(title,fontproperties =zhfont1, fontsize=40)    # 图像标题
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]    # normalization
+    plt.imshow(cm, interpolation='nearest')    # show image
+    plt.title(title,fontproperties =zhfont1, fontsize=40)    # caption
     plt.colorbar()
     num_local = np.array(range(20))    
-    plt.xticks(num_local, num_local, rotation=90)    # 将标签印在x轴坐标上
-    plt.yticks(num_local, num_local)    # 将标签印在y轴坐标上
-    plt.ylabel('真实标签',fontproperties =zhfont1, fontsize=40)   #y轴名称 
-    plt.xlabel('预测标签',fontproperties =zhfont1, fontsize=40)   #x轴名称
+    plt.xticks(num_local, num_local, rotation=90)    # label in x axis
+    plt.yticks(num_local, num_local)    # label in y axis上
+    plt.ylabel('ground true',fontproperties =zhfont1, fontsize=40)   #name of y axis 
+    plt.xlabel('prediction',fontproperties =zhfont1, fontsize=40)   #name of x axis
     for i in num_local:
 #        for j in num_local:
         plt.text(i, i, str('%.2f' % (cm[i, i])), va='center', ha='center')
 
 cm1 = np.load('./confusion_matrix/comps_sv_20(3)_3_pre.npy')
-plot_confusion_matrix(cm1, "初始阶段")#这里输入的是图片的名称
-plt.savefig('./conf_pic/1.png', format='png')  #保存文件名称
+plot_confusion_matrix(cm1, "pre-train")#image name
+plt.savefig('./conf_pic/1.png', format='png')  #file name
 plt.show()
